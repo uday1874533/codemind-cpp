d38a9143b85d0f64cba89f4297383647b9e27856
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-23 19:01:24 +0530

    #include <stdio.h> #include <math.h> int main() { 	int n; 	scanf("%d",&n); 	float m=sqrt(n); 	if(m==(int)m) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }
    // .kugcw54ep9t ljg igpyf57

---
+++ perfect_square_root_or_not.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	float m=sqrt(n);
+	if(m==(int)m)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
+
+// .kugcw54ep9t ljg igpyf57 
